d93e3a3287b644b113dcb7fa419b59c4c806f120
AtinAgnihotri <atinagnihotri@gmail.com>
2021-09-09 09:34:36 +0530

    Initial Commit

---
+++ StocksClone/StocksCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksCloneApp.swift
+//  StocksClone
+//
+//  Created by Atin Agnihotri on 09/09/21.
+//
+
+import SwiftUI
+
+@main
+struct StocksCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
