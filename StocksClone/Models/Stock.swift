246b845ec66d3c2ae189e2e5aecfb16def035497
AtinAgnihotri <atinagnihotri@gmail.com>
2021-09-09 10:16:57 +0530

    Adds Stock model

---
+++ StocksClone/Models/Stock.swift
@@ -0,0 +1,15 @@
+//
+//  Stock.swift
+//  StocksClone
+//
+//  Created by Atin Agnihotri on 09/09/21.
+//
+
+import Foundation
+
+struct Stock {
+    let symbol: String
+    let description: String
+    let price: Double
+    let change: String
+}
